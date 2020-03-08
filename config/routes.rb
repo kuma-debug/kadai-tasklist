688324079a686b7eff1702665b3a7cc72e6c1c39
kuma-debug <ntkumagai1@gmail.com>
2020-03-08 05:07:34 +0000

    users_contrller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-#  root to: 'toppages#index'
   root to: 'tasks#index'
 
   get 'login', to: 'sessions#new'
@@ -8,6 +7,7 @@ Rails.application.routes.draw do
   
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create]
+#  resources :users, only: [:new, :create]
 
   resources :tasks
 end

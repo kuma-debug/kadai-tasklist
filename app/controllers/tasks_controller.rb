7e93625d7aec69c10d92736f7ade6f6ec0ed9e14
kuma-debug <ntkumagai1@gmail.com>
2020-03-08 09:00:36 +0000

    err modify

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,9 @@
 class TasksController < ApplicationController
-  before_action :require_user_logged_in, only: [:show, :new, :create, :edit, :update, :destroy]
-  before_action :correct_user, only: [:destroy]
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
 
   def index
-    if logged_in?
-      @tasks = current_user.tasks.order(id: :desc).page(params[:page]).per(5)
-    end
+    @tasks = current_user.tasks.order(id: :desc).page(params[:page]).per(5)
   end
 
   def show
@@ -47,10 +44,6 @@ class TasksController < ApplicationController
   end
 
  private
-  def set_task
-    @task = current_user.tasks.find(params[:id])
-  end
-
   # Strong Parameter
   def task_params
     params.require(:task).permit(:content, :status)

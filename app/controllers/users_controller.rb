b8767c6f607f69d5b9aef6736229cc451f54f493
kuma-debug <ntkumagai1@gmail.com>
2020-03-08 05:19:11 +0000

    little modify

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,9 +18,7 @@ class UsersController < ApplicationController
 
     if @user.save
       flash[:success] = 'ユーザを登録しました。'
-#      redirect_to @user
-#      redirect_to @user
-      redirect_to tasks_path
+      redirect_to root_url
     else
       flash.now[:danger] = 'ユーザの登録に失敗しました。'
       render :new
